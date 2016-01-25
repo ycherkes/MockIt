fbaedcea0d9155a339265d8d232db4475d07734c
smurtdev <smurtdev@gmail.com>
2016-01-25 21:58:08 +0200

    remove unneeded usings

--- MockIt/MockIt/TestMethodCodeFixProvider.cs
+++ MockIt/MockIt/TestMethodCodeFixProvider.cs
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
