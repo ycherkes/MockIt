555d57b8f7bf1dd3338042946c65a2b27f76ef38
ycherkes <smurtdev@gmail.com>
2016-01-26 17:56:23 +0200

    fix Generics

--- MockIt/MockIt/FriendlyNamesHelper.cs
+++ MockIt/MockIt/FriendlyNamesHelper.cs
@@ -15,38 +15,16 @@
 // 
 // The latest version of this file can be found at https://github.com/ycherkes/MockIt
 #endregion
-using System.Collections.Generic;
+
 using Microsoft.CodeAnalysis;
 
 namespace MockIt
 {
     public class FriendlyNamesHelper
     {
-        private static readonly Dictionary<string, string> primitiveTypes = new Dictionary<string, string>
-        {
-            { typeof(object).Name,  "object"} ,
-            { typeof(string).Name,  "string"} ,
-            { typeof(bool).Name,    "bool"} ,
-            { typeof(byte).Name,    "byte"} ,
-            { typeof(char).Name,    "char"} ,
-            { typeof(decimal).Name, "decimal"} ,
-            { typeof(double).Name,  "double "} ,
-            { typeof(short).Name,   "short"} ,
-            { typeof(int).Name,     "int"} ,
-            { typeof(long).Name,    "long"} ,
-            { typeof(sbyte).Name,   "sbyte"} ,
-            { typeof(float).Name,   "float"} ,
-            { typeof(ushort).Name,  "ushort"} ,
-            { typeof(uint).Name,    "uint"} ,
-            { typeof(ulong).Name,   "ulong"}
-        };
-
-      
         public static string GetSimpleTypeName(ISymbol type)
         {
-            string result;
-
-            return primitiveTypes.TryGetValue(type.Name, out result) ? result : type.Name;
+            return type.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
         }
     }
 }
