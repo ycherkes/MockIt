f313539f126b1d434f41d8ca8827ea1c2911324a
ycherkes <smurtdev@gmail.com>
2016-02-01 17:15:08 +0200

    simple properties access analyzing support

---
+++ MockIt/MockIt/SyntaxNodeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MockIt
+{
+    internal static class SyntaxNodeExtensions
+    {
+        public static bool IsLeftSideOfAssignExpression(this SyntaxNode node)
+        {
+            return node.IsParentKind(SyntaxKind.SimpleAssignmentExpression) &&
+                ((AssignmentExpressionSyntax)node.Parent).Left == node;
+        }
+
+        public static bool IsParentKind(this SyntaxNode node, SyntaxKind kind)
+        {
+            return node != null && node.Parent.IsKind(kind);
+        }
+
+        public static bool IsKind(this SyntaxNode node, SyntaxKind kind)
+        {
+            return node?.RawKind == (int)kind;
+        }
+    }
+}
